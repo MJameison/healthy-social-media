c6018e6687438d09433ca13c0423b3e01f30d36a
Matthew Jameison <matthewjam46@gmail.com>
2021-12-06 17:04:22 +0000

    Added notification display to user in app

--- PostboxNotification/app/src/main/java/com/example/postboxnotification/InterceptionService.java
+++ PostboxNotification/app/src/main/java/com/example/postboxnotification/InterceptionService.java
@@ -1,26 +1,63 @@
 package com.example.postboxnotification;
 
-import android.content.Context ;
-import android.service.notification.NotificationListenerService ;
-import android.service.notification.StatusBarNotification ;
-import android.util.Log ;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+
 public class InterceptionService extends NotificationListenerService {
-    private final String TAG = this .getClass().getSimpleName() ;
-    Context context ;
+    private final String TAG = this .getClass().getSimpleName();
+    Context context;
+
+    private boolean blockingEnabled = true;
+    protected static InterceptionService instance;
+
+    private ArrayList<StatusBarNotification> postbox;
 
     @Override
     public void onCreate () {
-        super .onCreate() ;
-        context = getApplicationContext() ;
+        super .onCreate();
+        context = getApplicationContext();
+        instance = this;
+        postbox = new ArrayList<>();
     }
     @Override
     public void onNotificationPosted (StatusBarNotification sbn) {
-        Log.i(TAG ,"Posted ID:" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
-        // Here's how we can kill notifications of any type
-        //cancelNotification(sbn.getKey());
+        Log.i(TAG ,"Posted ID:" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName() + "\t" + sbn.getKey());
+
+        // Are we currently in an un-snoozing window?
+
+        // Check if we should block the notification
+        if (blockingEnabled && shouldBlock(sbn)) {
+            // Calculate time to next display period
+            //long snoozeMillis = 10000;
+            //snoozeNotification(sbn.getKey(), snoozeMillis);
+
+            postbox.add(sbn);
+        }
+
     }
     @Override
     public void onNotificationRemoved (StatusBarNotification sbn) {
         Log.i(TAG ,"Removed ID:" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
+    }
+
+    public StatusBarNotification[] getNotifications() {
+        return postbox.toArray(new StatusBarNotification[0]);
+    }
+
+    public void clearPostbox() {
+        postbox.clear();
+    }
+
+    private boolean shouldBlock(StatusBarNotification sbn) {
+        // Blocking logic goes here
+
+        return true;
     }
 }
--- PostboxNotification/app/src/main/java/com/example/postboxnotification/MainActivity.java
+++ PostboxNotification/app/src/main/java/com/example/postboxnotification/MainActivity.java
@@ -11,15 +11,26 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private NotificationManagerCompat notificationManager;
     private EditText editTextTitle;
     private EditText editTextMessage;
+    private LinearLayout linearScroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +40,12 @@ public class MainActivity extends AppCompatActivity {
         notificationManager = NotificationManagerCompat.from(this);
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextMessage = findViewById(R.id.edit_text_message);
+        linearScroll = findViewById(R.id.linear_layout_scroll);
 
         if(!isNotificationServiceEnabled()) {
             buildNotificationServiceAlertDialog().show();
         }
+
     }
 
     public void sendOnChannel1(View v) {
@@ -59,7 +72,74 @@ public class MainActivity extends AppCompatActivity {
         notificationManager.notify(2, notification);
     }
 
+    public void collectMail(View v) {
+        StatusBarNotification[] sbns = InterceptionService.instance.getNotifications();
 
+        //Clear the existing display
+        linearScroll.removeAllViews();
+
+        // If we don't have any messages display the empty message
+        if(sbns.length == 0) {
+            Log.i("Output", "Empty!!!");
+
+            TextView empty = new TextView(this);
+            empty.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            empty.setText(R.string.no_mail_text);
+            linearScroll.addView(empty);
+
+            return;
+        }
+
+        for (StatusBarNotification sbn : sbns) {
+            Log.i("Output", sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName() + "\t" + sbn.getKey());
+
+            Bundle extras = sbn.getNotification().extras;
+
+
+            // Horizontal Layout
+            LinearLayout notif = new LinearLayout(this);
+            notif.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            notif.setOrientation(LinearLayout.HORIZONTAL);
+            linearScroll.addView(notif);
+
+            // Add image to LHS
+            // TODO: Get notification image
+            ImageView small_icon = new ImageView(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(100, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.gravity = Gravity.CENTER_VERTICAL;
+            small_icon.setLayoutParams(params);
+            small_icon.setImageResource(R.drawable.ic_one);
+            notif.addView(small_icon);
+
+            // Add LinearLayout to RHS
+            LinearLayout details = new LinearLayout(this);
+            details.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            details.setOrientation(LinearLayout.VERTICAL);
+            notif.addView(details);
+
+            // Add app title
+            TextView app = new TextView(this);
+            Log.i("OUTPUT", sbn.getPackageName());
+            app.setText(sbn.getPackageName());
+            app.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            details.addView(app);
+
+            // Add notif title
+            TextView title = new TextView(this);
+            Log.i("OUTPUT", extras.getString(Notification.EXTRA_TITLE));
+            title.setText(extras.getString(Notification.EXTRA_TITLE));
+            title.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            details.addView(title);
+
+            // Add notif content
+            TextView content = new TextView(this);
+            Log.i("OUTPUT", extras.getString(Notification.EXTRA_TEXT));
+            content.setText(extras.getString(Notification.EXTRA_TEXT));
+            content.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            details.addView(content);
+        }
+        InterceptionService.instance.clearPostbox();
+    }
 
     private boolean isNotificationServiceEnabled(){
         String pkgName = getPackageName();
